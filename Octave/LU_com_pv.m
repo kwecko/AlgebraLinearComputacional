f3b9432c8b7c6b58f26a8ae380e480f6dbc30291
Marcelo Rios Kwecko <59372283+kwecko@users.noreply.github.com>
2022-06-24 12:13:48 -0300

    Update LU_com_pv.m

--- Octave/LU_com_pv.m
+++ Octave/LU_com_pv.m
@@ -7,31 +7,6 @@ clear all;
 % Amazena o tempo de CPU inicial
 t=cputime;
 
-% Funcoes
-% -------
-
-function [x]=solveL(L,b)   #L: matriz triangular inferior
-  n=size(L,1);              #b: vetor
-  x(1)=b(1)/L(1,1);
-
-  for i=2:n
-      x(i)=(b(i)-L(i,1:i-1)*x(1:i-1)')/L(i,i);
-  end
-  x'(i);
-endfunction
-
-function [x]=solveUP(U,b)    #U: matriz triangular superior
-   n=length(b);              #b: vetor
-   x=zeros(n,1);
-   for k=n:-1:1;
-   x(k)=b(k)/U(k,k);
-    for i=1:k-1
-        b(i)=b(i)-x(k)*U(i,k);
-    end
-   end
-endfunction
-
-
 printf('\n\n Fatoracao LU sem Pivotamento \n')
 fprintf('-------------------------------- \n')
 
